527229dc94a6c9ce0364ad9fe5cb8da9dbc5ec03
Alexander Murray-Watters <swiftwind1230@gmail.com>
2018-09-20 10:42:13 +0200

    Created minimal wrapper function in R for UMAP dimension reduction procedure.

---
+++ umap_wrapper.R
@@ -0,0 +1,29 @@
+## Minimal UMAP wrapper 
+
+## Install UMAP for python by following the instructions at: https://github.com/lmcinnes/umap, then use the code below.
+
+library(reticulate)
+
+umap.main <- import("umap")
+
+## New version using reticulate.
+umap <- function(x, n_neighbors = 10, min_dist = 0.1,
+                 metric = 'euclidean', y = c(),
+                 n_components = 2, ...){
+
+    ## Have to convert in order to ensure reticulate gives 
+    ## UMAP the right kind of python objects.
+    data <- as.matrix(x)
+    n_neighbors <- as.integer(n_neighbors)
+    n_components <- as.integer(n_components)
+
+    ## Intiallizing umap object with var. parameters.
+    umap.obj <- umap.main$UMAP(n_neighbors = n_neighbors,
+                               min_dist = min_dist,
+                               metric = metric,
+                               n_components = n_components,
+                               ...)
+
+    return(umap.obj$fit_transform(data, y = c()))
+}
+
